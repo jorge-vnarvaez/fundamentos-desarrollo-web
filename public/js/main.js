5815fddeca51ec342b1b558d0acf122ec80b4c64
jorge-vnarvaez <jorge.vnarvaez@gmail.com>
2026-07-24 09:23:50 -0400

    añadido campo correo con addEventListener

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,10 @@
-alert("Hola Git!")
+// DOM
+const correo = document.getElementById("correo")
+
+const formulario = document.getElementById("formulario")
+
+formulario.addEventListener("submit", (event) => {
+    event.preventDefault()
+    // Escribe tu código aquí debajo
+    console.log(correo.value)
+})
